a7304841654f52364701f47a20b3722c7bcdddc7
Sargorus <Dima35206@yandex.ru>
2026-03-04 11:11:13 +0800

    Добавлен метод Logic с логикой решения, и добавен ввод значений в main. Добавлен вывод ответа сообщением.

--- С#String1/Program.cs
+++ С#String1/Program.cs
@@ -1,23 +1,32 @@
-﻿namespace С_String1
+﻿using System;
+
+namespace С_String1
 {
     public class Logic
     {
-        public static string CompareTrainInStation(int ArrivalHour, int ArrivalMinute, int DepartureHour, int DepartureMinute, int PassengerHour, int PassengerMinute)
+        public static double PercentageLetters(string inputSentence)
         {
-            int TimeArrive = ArrivalHour * 60 + ArrivalMinute;
-            int TimeDeparture = DepartureHour * 60 + DepartureMinute;
-            int TimePassenger = PassengerHour * 60 + PassengerMinute;
-
-            if (TimeArrive >= TimeDeparture) return "Время прибытия позже времени отбытия или равно ему!";
-            if (TimePassenger < TimeDeparture && TimePassenger > TimeArrive)
+            string sentence = inputSentence;
+            if (string.IsNullOrEmpty(sentence))
             {
-                return "Поезд находится на станции";
-            }
-            else
-            {
-                return "Поезд не находится на станции";
+                return -1; // будет кодом ошибки: путое предложение
             }
 
+            int letterCount = 0;
+            int lengthSentence = 0;
+            foreach (char symbolChar in sentence)
+            {
+                if (char.IsLetter(symbolChar))
+                {
+                    letterCount += 1;
+                }
+                lengthSentence += 1;
+            }
+
+            double percentage = ((double)letterCount / (double)lengthSentence) * 100;
+            // Console.WriteLine($"Количество букв = : {letterCount}");
+            // Console.WriteLine($"Длинна предложения  = : {lengthSentence}");
+            return percentage;
         }
 
     }
@@ -26,29 +35,12 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите час прибытия поезда:");
-            var ArrivalHour = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите предложение:");
+            string inputSentence = Console.ReadLine();
 
-            Console.Write("Введите минуту(ы) прибытия поезда:");
-            var ArrivalMinute = int.Parse(Console.ReadLine());
+            double percentage = Logic.PercentageLetters(inputSentence);
 
-            Console.Write("Введите час отбытия поезда:");
-            var DepartureHour = int.Parse(Console.ReadLine());
-
-            Console.Write("Введите минуту(ы) отбытия поезда:");
-            var DepartureMinute = int.Parse(Console.ReadLine());
-
-            Console.Write("Введите час прихода на станцию пассажира:");
-            var PassengerHour = int.Parse(Console.ReadLine());
-
-            Console.Write("Введите минуту(ы) прихода на станцию пассажира:");
-            var PassengerMinute = int.Parse(Console.ReadLine());
-
-            var resultMSG = Logic.CompareTrainInStation(ArrivalHour, ArrivalMinute, DepartureHour, DepartureMinute, PassengerHour, PassengerMinute);
-
-
-            Console.WriteLine(resultMSG);
-
+            Console.WriteLine($"Доля букв в предложении: {percentage} процентов");
         }
     }
 }
